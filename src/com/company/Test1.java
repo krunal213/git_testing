34d604ef22e4780e2c3405119ae1ddd8547c8a96
krunal21041993 <kathikar.krunal3@gmail.com>
2024-08-18 13:29:51 +0530

    Completed Test class

--- src/com/company/Test1.java
+++ src/com/company/Test1.java
@@ -1,4 +1,18 @@
 package com.company;
 
 public class Test1 {
+
+    private int propertyOne;
+
+    public Test1(int propertyOne) {
+        this.propertyOne = propertyOne;
+    }
+
+    public int getPropertyOne() {
+        return propertyOne;
+    }
+
+    public void setPropertyOne(int propertyOne) {
+        this.propertyOne = propertyOne;
+    }
 }
